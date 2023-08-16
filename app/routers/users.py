ed556bf94ac803056cb47c3612db69f95cb730a9
jpdotdev <johndpezza@gmail.com>
2023-08-16 10:49:27 -0400

    added delete user route to backend

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,12 @@
-from .. import models, schemas, utils
-from fastapi import status, HTTPException, Depends, APIRouter
+from .. import models, schemas, utils, oauth2
+from fastapi import status, HTTPException, Depends, APIRouter, Response
 from ..database import get_db
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix='/users',
                    tags=['Users'])
 
-
+# Create a user
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
@@ -21,6 +21,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
+# Get user profile
 @router.get('/{id}', response_model=schemas.UserResponse)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
@@ -30,4 +31,27 @@ def get_user(id: int, db: Session = Depends(get_db)):
                             detail=f"User with id {id} was not found")
     return user
 
+
+#delete user
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, db: Session = Depends(get_db),
+                current_user: int = Depends(oauth2.get_current_user)):
+    
+    user = db.query(models.User).filter(models.User.id == id)
+    deleted_user = user.first()
+
+    if deleted_user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There is no user with id: {id} to delete")
+
+    
+    if deleted_user.id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action")
+    
+    
+    user.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
